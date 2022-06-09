3f0515a334eccf70275510417c1f4435d41d68c2
FrancoLF95 <107162065+FrancoLF95@users.noreply.github.com>
2022-06-08 21:27:29 -0500

    Resolución_Proyecto_Final

---
+++ democlientesafp/src/main/java/com/nttdata/clientesafp/democlientesafp/DemoclientesafpApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.clientesafp.democlientesafp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoclientesafpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoclientesafpApplication.class, args);
+	}
+
+}
---
+++ democlientesafp/src/test/java/com/nttdata/clientesafp/democlientesafp/DemoclientesafpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.clientesafp.democlientesafp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoclientesafpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
